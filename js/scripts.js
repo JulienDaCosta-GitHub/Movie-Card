c4667e135f20902e28f02a0543cbaa016f7ac82a
JulienDaCosta-GitHub <juliendacosta27@gmail.com>
2019-12-03 15:22:40 +0100

    Test 5
    Other tests in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -73,36 +73,54 @@
 
 // Test 4 Un début
 
-let baseURL = 'https://api.themoviedb.org/3/';
-let configData = null;
-let ImageURL = null;
-APIKEY = '6b792f50e68dd0cf7abebb1399199bb2';
+// let baseURL = 'https://api.themoviedb.org/3/';
+// let configData = null;
+// let ImageURL = null;
+// APIKEY = '6b792f50e68dd0cf7abebb1399199bb2';
 
-let getConfig = function () {
-    let url = "".concat(baseURL, 'configuration?api_key=', APIKEY);
-    fetch(url)
-        .then((result) => {
-            return result.json();
-        })
-        .then((data) => {
-            baseImageURL = data.images.secure_base_url;
-            configData = data.images;
-            console.log('config:', data)
-            console.log('config fetched')
-            runSearch('Iron man')
-        })
-        .catch(function (err) {
-            alert(err);
-        });
-}
+// let getConfig = function () {
+//     let url = "".concat(baseURL, 'configuration?api_key=', APIKEY);
+//     fetch(url)
+//         .then((result) => {
+//             return result.json();
+//         })
+//         .then((data) => {
+//             baseImageURL = data.images.secure_base_url;
+//             configData = data.images;
+//             console.log('config:', data)
+//             console.log('config fetched')
+//             runSearch('Iron man')
+//         })
+//         .catch(function (err) {
+//             alert(err);
+//         });
+// }
 
-let runSearch = function (keyword) {
-    let url = ''.concat(baseURL, 'search/movie?api_key=', APIKEY, '&query=', keyword);
-    fetch(url)
-        .then(result => result.json())
-        .then((data) => {
-            document.getElementById('Popular-Movies-List').innerHTML = JSON.stringify(data, null, 4);
-        })
-}
+// let runSearch = function (keyword) {
+//     let url = ''.concat(baseURL, 'search/movie?api_key=', APIKEY, '&query=', keyword);
+//     fetch(url)
+//         .then(result => result.json())
+//         .then((data) => {
+//             document.getElementById('Popular-Movies-List').innerHTML = JSON.stringify(data, null, 4);
+//         })
+// }
 
-document.addEventListener('DOMContentLoaded', getConfig);
+// document.addEventListener('DOMContentLoaded', getConfig);
+
+
+
+// Test 5 fail
+
+// let PopularMovies = document.querySelector('#Popular-Movies-List')
+
+// function getTitles() {
+//     axios.get('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6b792f50e68dd0cf7abebb1399199bb2').then(function (response) {
+//         updateTitles(response.data) // Fonction pour appeler les attributs
+//     })
+// }
+
+// function updateTitles(data) {
+//     PopularMovies.innerText = data.page.results.original_title
+// }
+
+// PopularMovies.addEventListener('click', getTitles)
