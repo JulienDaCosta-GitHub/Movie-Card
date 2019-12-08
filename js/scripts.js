2f154aa91b6338d6ecc3fc659eb8cb44cd70cf58
JulienDaCosta-GitHub <juliendacosta27@gmail.com>
2019-12-08 19:51:26 +0100

    Final update
    New style for the website (new colours and fonts)

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ function GetPopularMovies() {
     var url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6b792f50e68dd0cf7abebb1399199bb2";
 
     $.get(url, callBackGetSuccess).done(function (SelectData) {
-        // Succès de la requète / Première couche de traitement
+
         var ListMovies = SelectData['results'];
         var PopularShowAllData = "";
 
@@ -20,18 +20,17 @@ function GetPopularMovies() {
                 "<img src='https://image.tmdb.org/t/p/w185" + movie.poster_path + "'>" +
                 "<br>" +
                 movie.original_title +
-                "</div><br>";
+                "</div>";
         });
 
         $('#Popular-Movies-List').html(PopularShowAllData);
 
     })
         .fail(function () {
-            // Cas d'erreur
 
         })
         .always(function () {
-            // Application de la deuxième couche de traitement
+
         });
 }
 
@@ -45,7 +44,7 @@ function GetRatingMovie() {
     console.log(url2 + inputtitle + apikeyurl2);
 
     $.get(url2 + inputtitle + apikeyurl2, callBackGetSuccess2).done(function (SelectRatingData) {
-        // Succès de la requète / Première couche de traitement
+
         var FindRating = SelectRatingData['imdbRating']
         var RatingShowAllData = "";
 
@@ -69,11 +68,11 @@ function GetRatingMovie() {
 
     })
         .fail(function () {
-            // Cas d'erreur
+
 
         })
         .always(function () {
-            // Application de la deuxième couche de traitement
+
         });
 }
 
